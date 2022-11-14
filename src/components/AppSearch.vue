48c957b84896cdfb594ba7f47f1c180e67fe202f
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-14 17:12:27 +0100

    file AppSearch.vue

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from "../store";
+
+export default {
+    name: "AppSearch",
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="search-box">
+        <input type="text" class="form-control" placeholder="Inserisci nome Film" v-model="store.searchInput">
+        <button class="btn" @click="$emit('startSearch')">Find</button>
+    </div>
+</template>
